c603efbdd282e9814e321ce8b1e9a395698bf5a1
deysandip301 <deysandip301@gmail.com>
2025-09-21 18:06:55 +0530

    Add Factory pattern for strategy creation
    - Implement WinningStrategyFactory for creating winning strategies
    - Implement KillingStrategyFactory for creating killing strategies
    - Use enum-based factory method pattern for type safety
    - Centralize strategy creation logic for better maintainability
    - Enable easy addition of new strategy types
    - Improve separation of concerns between strategy creation and usage

---
+++ src/main/java/com/game/factories/KillingStrategyFactory.java
@@ -0,0 +1,17 @@
+package com.game.factories;
+
+import com.game.strategies.killing.*;
+
+public class KillingStrategyFactory {
+
+    public static KillingStrategy createKillingStrategy(KillingStrategyType type) {
+        switch (type) {
+            case DEFAULT:
+                return new DefaultKillingStrategy();
+            case START_AGAIN:
+                return new StartAgainKillingStrategy();
+            default:
+                throw new IllegalArgumentException("Unknown killing strategy type: " + type);
+        }
+    }
+}
---
+++ src/main/java/com/game/factories/WinningStrategyFactory.java
@@ -0,0 +1,17 @@
+package com.game.factories;
+
+import com.game.strategies.winning.*;
+
+public class WinningStrategyFactory {
+
+    public static WinningStrategy createWinningStrategy(WinningStrategyType type) {
+        switch (type) {
+            case DEFAULT:
+                return new DefaultWinningStrategy();
+            case EXACT_MATCH:
+                return new DefaultWinningStrategy(); // Could be different implementation
+            default:
+                throw new IllegalArgumentException("Unknown winning strategy type: " + type);
+        }
+    }
+}
